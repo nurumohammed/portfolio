eefaf44ea8caad8d243e36e9980130be4da7e52f
nurumohammed <meetnurumd5520@gmail.com>
2024-04-16 14:47:21 +0300

    first

---
+++ src/containers/resume/db.js
@@ -0,0 +1,25 @@
+export const Data = {
+
+    Experiance:[
+        {
+            title: 'project Manager',
+            description:'Creative Direction project Management'
+        },
+        {
+            title:'sinier Consultant',
+            description:'web development ,Database Backend Development'
+        }
+    ],
+    
+     Education:[
+        {
+            title:'University',
+            description:'wollo university'
+        },
+        {
+            title:'dept',
+            description: 'Computer science'
+        }
+            
+        ]
+}
